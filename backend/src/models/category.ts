7c81342307a3cbf980558b33794d6de3506a355e
AlexCuadra <alex.cuadra@ahinko.com>
2022-12-21 09:51:16 -0600

    feature: adding things to the model of categories

--- backend/src/models/category.ts
+++ backend/src/models/category.ts
@@ -8,6 +8,20 @@ const categorySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// categorySchema.virtual("items", { ref: "Item", localField: "_id", foreignField: "category" });
+
+categorySchema.methods.toJSON = function () {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const category = this;
+  const categoryObject = category.toObject();
+
+  delete categoryObject.__v;
+  delete categoryObject.updatedAt;
+  delete categoryObject.createdAt;
+
+  return categoryObject;
+};
+
 const Categories = mongoose.model("Category", categorySchema);
 
 export default Categories;
