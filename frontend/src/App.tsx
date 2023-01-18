04bc9095eb797e160078a53cbc36adf5e857c25d
AlexC01 <acuadraq@gmail.com>
2023-01-18 11:26:38 -0600

    styles: adding multiple styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div className="flex h-screen antialiased bg-bgmain">
+    <div className="flex h-screen antialiased bg-bgmain font-quicksand">
       <Sidebar />
       <Outlet />
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily: {},
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
       colors: {
         bgmain: "#FAFAFE",
         cartbg: "#F9A109",
+        redbox: "#EB5757",
+        greybg: "#454545",
       },
     },
   },
