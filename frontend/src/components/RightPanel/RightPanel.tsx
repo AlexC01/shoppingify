2cb8b01d06015801677d46e83e219beeec8c318b
AlexC01 <acuadraq@gmail.com>
2023-01-29 00:40:39 -0600

    feature: adding feedback when no items are added to the cart

--- frontend/src/components/RightPanel/RightPanel.tsx
+++ frontend/src/components/RightPanel/RightPanel.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import bottle from "../../assets/source.svg";
+import shop from "../../assets/shop.svg";
 import GeneralContext from "../../context/GeneralContext";
 import { createCart, getCart } from "../../helpers/Cart";
 import { CartItemsArr, CartResponse, CreateCartObj } from "../../models/Cart";
@@ -11,7 +12,9 @@ interface Props {
 }
 
 const RightPanel = ({ token }: Props) => {
-  const { GlobalContext } = useContext(GeneralContext) as GlobalContextType;
+  const { GlobalContext, setGlobalContext } = useContext(
+    GeneralContext,
+  ) as GlobalContextType;
   const [loading, setLoading] = useState(false);
   const [currentCartId, setCurrentCartId] = useState("");
   const [cart, setCart] = useState<CartResponse>();
@@ -41,6 +44,7 @@ const RightPanel = ({ token }: Props) => {
       }
       setCart(resp[0]);
       setCartItems(resp[0].cart_items);
+      setGlobalContext({ ...GlobalContext, items: resp[0].cart_items.length });
       setLoading(false);
     } catch (err) {
       console.log(err);
@@ -59,26 +63,40 @@ const RightPanel = ({ token }: Props) => {
     <section className="fixed inset-y-0  right-0 z-10 flex-shrink-0 w-96">
       <div className="bg-creambg h-full w-full pt-10">
         <div className="px-10 flex flex-col h-full">
-          <div className="bg-redwine mx-auto h-32 gap-10 rounded-2xl px-5 relative grid grid-cols-3">
-            <div className="-mt-5 w-20">
-              <img src={bottle} alt="Botella" />
-            </div>
-            <div className="col-span-2 my-auto">
-              <p className="text-sm font-semibold mb-3 text-white">
-                Didn&apos;t find what you need?
-              </p>
-              <button
-                type="button"
-                className="bg-white px-4 py-2 text-sm rounded-lg font-semibold"
-              >
-                Add Item
-              </button>
+          <div className="flex-1">
+            <div className="bg-redwine mx-auto h-32 gap-10 rounded-2xl px-5 relative grid grid-cols-3">
+              <div className="-mt-5 w-20">
+                <img src={bottle} alt="Botella" />
+              </div>
+              <div className="col-span-2 my-auto">
+                <p className="text-sm font-semibold mb-3 text-white">
+                  Didn&apos;t find what you need?
+                </p>
+                <button
+                  type="button"
+                  className="bg-white px-4 py-2 text-sm rounded-lg font-semibold"
+                >
+                  Add Item
+                </button>
+              </div>
             </div>
           </div>
           {loading && (
             <div className="flex items-center justify-center flex-1">
               <Spinner />
             </div>
+          )}
+          {!loading && cartItems.length === 0 && (
+            <>
+              <div className="flex items-center justify-center flex-1">
+                <h3 className="text-textbrown font-bold tracking-wider text-lg">
+                  No Items
+                </h3>
+              </div>
+              <div className="flex items-center justify-center flex-1">
+                <img src={shop} alt="Botella" />
+              </div>
+            </>
           )}
         </div>
       </div>
